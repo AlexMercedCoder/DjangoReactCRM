e0c106360afce55748f85d134680c00008811cf3
Alex Merced <WillCodeforFood@AlexMerced.com>
2019-09-08 16:22:55 -0400

    React app is now loading, now time to design front endgit add .!

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,27 +2,16 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
  
  
-export class App extends Component {
+class App extends Component {
  
-  state = {
-      data: ''
-  };
- 
-    componentDidMount() {
-        fetch("/api/actor/")
-          .then(response => {
-          return response.json();
-        })
-        .then(data => this.setState({ data: JSON.stringify(data)}));
-  }
  
   render(){
  
       return (
-            <p>Jason data = {this.state.data}  test text</p>
+            <h1> Hello World!</h1>
       )
   }
 }
  
  
-wrapper ? ReactDOM.render(<App></App>, wrapper) : null;
+ReactDOM.render(<App/>, document.getElementById('app'));
